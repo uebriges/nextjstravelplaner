b7970986487c7ec32a87ce06f7c1e6e4f59519d9
Uebriges <uebriges@gmx.at>
2021-04-07 18:26:22 +0200

    Increase timeout for rendering the map

--- cypress/integration/e2eTrips/createTrip.spec.js
+++ cypress/integration/e2eTrips/createTrip.spec.js
@@ -2,7 +2,7 @@ export default function CreateNewTrip() {
   describe('Create new trip by setting waypoints on the map', () => {
     it('Add first waypoint', () => {
       // cy.visit('/travelplaner');
-      cy.wait(3000);
+      cy.wait(7000);
       cy.get('div.overlays').click(500, 500);
       cy.get('[data-cy="AddWaypointBtn"]').click();
       cy.wait(1200);
