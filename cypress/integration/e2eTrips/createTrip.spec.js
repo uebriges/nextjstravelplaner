1a3c9d723104ccd5911f47ecc06b968c5cfb5a67
Uebriges <uebriges@gmx.at>
2021-04-08 09:53:10 +0200

    Try to fix test issue on github actions

--- cypress/integration/e2eTrips/createTrip.spec.js
+++ cypress/integration/e2eTrips/createTrip.spec.js
@@ -3,18 +3,18 @@ export default function CreateNewTrip() {
     it('Add first waypoint', () => {
       // cy.visit('/travelplaner');
       cy.wait(7000);
-      cy.get('div.overlays').click(500, 500);
+      cy.get('div.overlays').click(500, 500, { force: true });
       cy.get('[data-cy="AddWaypointBtn"]').click();
       cy.wait(1200);
-      cy.get('div.overlays').click(600, 100);
+      cy.get('div.overlays').click(600, 100, { force: true });
       cy.wait(1200);
-      cy.get('div.overlays').click(300, 400);
-      cy.get('[data-cy="AddWaypointBtn"]').click();
+      cy.get('div.overlays').click(300, 400, { force: true });
+      cy.get('[data-cy="AddWaypointBtn"]').click({ force: true });
       cy.wait(1200);
-      cy.get('div.overlays').click(600, 100);
+      cy.get('div.overlays').click(600, 100, { force: true });
       cy.wait(1200);
-      cy.get('div.overlays').click(500, 300);
-      cy.get('[data-cy="AddWaypointBtn"]').click();
+      cy.get('div.overlays').click(500, 300, { force: true });
+      cy.get('[data-cy="AddWaypointBtn"]').click({ force: true });
     });
   });
 }
